e13b2586928c64bac61922666cccba83c5217fc2
Guillermo Casal Caro <Guillermo.Casal@unit4.com>
2025-02-13 15:51:22 +0100

    pawns walking forward

--- src/components/ChessBoard.tsx
+++ src/components/ChessBoard.tsx
@@ -7,18 +7,25 @@ import { useDraggingPiece } from "../state/draggingPiece";
 export const BOARD_SIZE = 8;
 export const TILE_SIZE = 100;
 
-
-
 export const ChessBoard = () => {
-    const { piecePositions, initializePositions } = usePiecePositions();
-    const { draggingPieceId } = useDraggingPiece()
+    const { piecePositions, initializePositions, setPiecePosition } =
+        usePiecePositions();
+    const { draggingPieceId, clearDraggingPiece } = useDraggingPiece();
 
     useEffect(() => {
-        const initialPositions = Array.from({ length: 8 }).reduce((acc: Record<number, PieceType>, _, i) => {
-            acc[i] = { id: i, type: "pawn", tile: { x: i, y: 6 }, numberOfMoves: 0 };
-            return acc;
-        }, {});
-        initializePositions(initialPositions)
+        const initialPositions = Array.from({ length: 8 }).reduce(
+            (acc: Record<number, PieceType>, _, i) => {
+                acc[i] = {
+                    id: i,
+                    type: "pawn",
+                    tile: { x: i, y: 6 },
+                    numberOfMoves: 0,
+                };
+                return acc;
+            },
+            {}
+        );
+        initializePositions(initialPositions);
     }, [initializePositions]);
 
     return (
@@ -27,27 +34,51 @@ export const ChessBoard = () => {
             height={BOARD_SIZE * TILE_SIZE}
             options={{ backgroundColor: 0x222222 }}>
             <Container>
-
-                {Array.from({ length: BOARD_SIZE * BOARD_SIZE }).map((_, index) => {
-                    const row = Math.floor(index / BOARD_SIZE);
-                    const col = index % BOARD_SIZE;
-                    const isDestination = draggingPieceId?.allowedTiles.filter(t => t.x === col && t.y === row).length;
-                    let color = (row + col) % 2 === 0 ? "rgb(249,223,189)" : "rgb(96,56,20)";
-                    if (isDestination) {
-                        color = "rgb(255,255,0)";
+                {Array.from({ length: BOARD_SIZE * BOARD_SIZE }).map(
+                    (_, index) => {
+                        const row = Math.floor(index / BOARD_SIZE);
+                        const col = index % BOARD_SIZE;
+                        const isDestination =
+                            draggingPieceId?.allowedTiles.filter(
+                                (t) => t.x === col && t.y === row
+                            ).length;
+                        let color =
+                            (row + col) % 2 === 0
+                                ? "rgb(249,223,189)"
+                                : "rgb(96,56,20)";
+                        if (isDestination) {
+                            color = "rgb(255,255,0)";
+                        }
+                        return (
+                            <Graphics
+                                key={`tile${col}${row}`}
+                                draw={(g) => {
+                                    g.beginFill(color);
+                                    g.drawRect(
+                                        col * TILE_SIZE,
+                                        row * TILE_SIZE,
+                                        TILE_SIZE,
+                                        TILE_SIZE
+                                    );
+                                    g.endFill();
+                                }}
+                                eventMode="static"
+                                onclick={() => {
+                                    console.log("cl");
+                                    if (!draggingPieceId || !isDestination) {
+                                        return;
+                                    }
+                                    setPiecePosition(
+                                        draggingPieceId.id,
+                                        col,
+                                        row
+                                    );
+                                    clearDraggingPiece();
+                                }}
+                            />
+                        );
                     }
-                    return (
-                        <Graphics
-                            key={index}
-                            draw={(g) => {
-                                g.beginFill(color);
-                                g.drawRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-                                g.endFill();
-                            }}
-                        />
-                    );
-                })}
-
+                )}
             </Container>
 
             {Object.values(piecePositions).map((piece) => {
@@ -58,7 +89,7 @@ export const ChessBoard = () => {
                         type={"pawn"}
                         position={piece.tile}
                     />
-                )
+                );
             })}
         </Stage>
     );
--- src/state/draggingPiece.tsx
+++ src/state/draggingPiece.tsx
@@ -1,15 +1,28 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
-export type Tile = { x: number, y: number }
+export type Tile = { x: number; y: number };
 
 type DraggingPieceStore = {
-    draggingPieceId: { id: number, allowedTiles: Tile[] } | null
-    setDraggingPieceId: (id: number, allowedTiles: Tile[]) => void
-    clearDraggingPiece: () => void
-}
+    draggingPieceId: { id: number; allowedTiles: Tile[] } | null;
+    setDraggingPieceId: (id: number | null, allowedTiles: Tile[]) => void;
+    clearDraggingPiece: () => void;
+};
 
 export const useDraggingPiece = create<DraggingPieceStore>()((set) => ({
     draggingPieceId: null,
-    setDraggingPieceId: (draggingPieceId, allowedTiles) => set({ draggingPieceId: { id: draggingPieceId, allowedTiles: allowedTiles } }),
-    clearDraggingPiece: () => set({ draggingPieceId: null })
-}))
+    setDraggingPieceId: (draggingPieceId, allowedTiles) => {
+        if (draggingPieceId === null) {
+            set({
+                draggingPieceId: null,
+            });
+            return;
+        }
+        set({
+            draggingPieceId: {
+                id: draggingPieceId,
+                allowedTiles: allowedTiles,
+            },
+        });
+    },
+    clearDraggingPiece: () => set({ draggingPieceId: null }),
+}));
