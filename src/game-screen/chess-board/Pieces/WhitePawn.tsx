7b8d8a5327c63488d4ec0083c94345b86e432c8a
Guillermo Casal Caro <guillermo.casal.caro@gmail.com>
2025-02-15 12:20:23 +0100

    undone texture

--- src/game-screen/chess-board/Pieces/WhitePawn.tsx
+++ src/game-screen/chess-board/Pieces/WhitePawn.tsx
@@ -1,14 +1,10 @@
-import { Container, Sprite, Text } from '@pixi/react';
+import { Sprite, Text } from '@pixi/react';
 import whitePawn from '/w_pawn.png';
-import bronzeTexturePng from '/bronze_texture.png';
 import { useDraggingPieceStore } from '../../../state/draggingPieceStore';
 import { usePiecePositionsStore } from '../../../state/piecePositionsStore';
 import { isTileOccupied, pruneOutboundTiles, tileToPixel } from './util';
 import { Tile } from './types';
-import { BLEND_MODES, TextStyle, Texture } from 'pixi.js';
-import { GlowFilter } from '@pixi/filter-glow';
-import { ColorMatrixFilter } from '@pixi/filter-color-matrix';
-import { AdjustmentFilter } from '@pixi/filter-adjustment';
+import { TextStyle } from 'pixi.js';
 
 interface PawnProps {
     id: string;
@@ -68,65 +64,32 @@ export const WhitePawn = ({ position, id, height, width }: PawnProps) => {
         return { allowedTiles, eatenTiles };
     };
 
-    const bronzeTexture = Texture.from(bronzeTexturePng);
-
-    // const filter = new GlowFilter({
-    //     distance: 10,
-    //     outerStrength: 0.5,
-    //     color: 0xffcc00,
-    //     innerStrength: 0.5,
-    //     quality: 2,
-    // });
-
-    // const colorFilter = new ColorMatrixFilter();
-    // colorFilter.hue(30, false);
-    // colorFilter.saturate(1.2);
-    // colorFilter.contrast(1.3, false);
-    // colorFilter.brightness(1.1, false);
-
-    // const adjustmentFilter = new AdjustmentFilter({
-    //     contrast: 1.4,
-    //     gamma: 1.1,
-    //     saturation: 1.2,
-    // });
-
     return (
         <>
-            <Container x={pixelPosition.x} y={pixelPosition.y}>
-                <Sprite
-                    image={whitePawn}
-                    x={pixelPosition.x}
-                    y={pixelPosition.y}
-                    height={height}
-                    width={width}
-                    anchor={0.5}
-                    filters={[]}
-                    eventMode="dynamic"
-                    onclick={() => {
-                        if (draggingPiece?.id === id) {
-                            clearDraggingPiece();
-                        } else {
-                            const moveTiles = getMoveTiles();
-                            moveTiles.allowedTiles = pruneOutboundTiles(
-                                moveTiles.allowedTiles,
-                            );
-                            setDraggingPiece(
-                                id,
-                                moveTiles.allowedTiles,
-                                moveTiles.eatenTiles,
-                            );
-                        }
-                    }}
-                />
-                {/* <Sprite
-                    texture={bronzeTexture}
-                    height={height}
-                    width={width}
-                    anchor={0.5}
-                    alpha={0.6}
-                    blendMode={BLEND_MODES.MULTIPLY}
-                /> */}
-            </Container>
+            <Sprite
+                image={whitePawn}
+                x={pixelPosition.x}
+                y={pixelPosition.y}
+                height={height}
+                width={width}
+                anchor={0.5}
+                eventMode="dynamic"
+                onclick={() => {
+                    if (draggingPiece?.id === id) {
+                        clearDraggingPiece();
+                    } else {
+                        const moveTiles = getMoveTiles();
+                        moveTiles.allowedTiles = pruneOutboundTiles(
+                            moveTiles.allowedTiles,
+                        );
+                        setDraggingPiece(
+                            id,
+                            moveTiles.allowedTiles,
+                            moveTiles.eatenTiles,
+                        );
+                    }
+                }}
+            />
             <Text
                 text={String(piecePositions[id].kills)}
                 anchor={0.5}
